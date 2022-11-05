f555cdbef1df7706bd611320db1ad6f4cc07ec48
vasamsettiamulya <vasamsettiamulya@gmail.com>
2022-11-05 21:11:44 +0530

    file sending

---
+++ Arithematic.java
@@ -0,0 +1,28 @@
+class Arithematic
+{
+public static void main(String args[])
+{
+int a=10,b=5,c;
+System.out.println("enter a="+a+"b="+b);
+c=a+b;
+{
+System.out.println("The addition of" +a+ "and" +b+ "is:"+c);
+}
+c=a-b;
+{
+System.out.println("The substraction of" +a+ "and" +b+ "is:"+c);
+}
+c=a*b;
+{
+System.out.println("The multiplication of" +a+ "and" +b+ "is:"+c);
+}
+c=a/b;
+{
+System.out.println("The division of" +a+ "and" +b+ "is:"+c);
+}
+c=a%b;
+{
+System.out.println("The modulus of"+a+ "and" +b+ "is:"+c);
+}
+}
+}
---
+++ Rectangle.java
@@ -0,0 +1,30 @@
+class Rectangle
+{
+int length;
+int width;
+void getdata(intl,intw)
+{
+length=l;
+width=w;
+}
+int calArea()
+{
+return (length*breath);
+}
+int calParmeter()
+{
+return (2(length+width));
+}
+}
+class Example
+{
+public static void main(String args[])
+{
+rectangle r1=new rectangle();
+r1.getdata(30,40);
+System.out.println("rectangle length:"+r1.length);
+System.out.println("rectangle width:"+r1.width);
+System.out.println("area is:"+r1.calarea());
+System.out.println("parameter is:"+r1.calparameter());
+}
+} 
