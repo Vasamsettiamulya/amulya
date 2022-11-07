61712afdb69685c8bd45b7ab22a4b0ee8e57a72f
vasamsettiamulya <vasamsettiamulya@gmail.com>
2022-11-07 19:33:08 +0530

    file sending

---
+++ string.java
@@ -0,0 +1,13 @@
+class StringExample
+{
+public static void main(String args[])
+{
+String S1="JAVA";
+char ch[]={'s','t','r','i','n','g'};
+String S2=new String(ch);
+String S3=new String("example");
+System.out.println(S1);
+System.out.println(S2);
+System.out.println(S3);
+}
+}
