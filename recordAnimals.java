167474cd1e99a9c3ae803fc5f0d4f95835322f7f
vasamsettiamulya <vasamsettiamulya@gmail.com>
2022-11-17 18:29:56 +0530

    record sending

---
+++ recordAnimals.java
@@ -0,0 +1,22 @@
+record animal(String name,String type,int livingage)
+{
+animal()
+{
+this("tiger","wildanimal",26);
+}
+public static void main(String args[])
+{
+Student S1=new Student("tiger","wildanimal",26);
+Student S2=new Student("dog" , "domasticanimal" ,16);
+Student S3=new Student("cat" ,"domasticanimal",14);
+
+
+System.out.println(S1);
+
+System.out.println(S2);
+
+System.out.println(S3);
+
+
+}
+}
