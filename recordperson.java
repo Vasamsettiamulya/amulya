efac3449a656c4a82aa1d6efbe5be1a505bf36aa
vasamsettiamulya <vasamsettiamulya@gmail.com>
2022-11-17 19:42:20 +0530

    record sending

---
+++ recordperson.java
@@ -0,0 +1,22 @@
+record person(String name,int age, String color)
+{
+person()
+{
+this("mahesh",47,"white");
+}
+public static void main(String args[])
+{
+Student S1=new Student("mahesh",47,"white");
+Student S2=new Student("prabhas",43,"lightcream");
+Student S3=new Student("ram",40,"white");
+
+
+System.out.println(S1);
+
+System.out.println(S2);
+
+System.out.println(S3);
+
+
+}
+}
