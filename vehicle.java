fc802083d4094955529588e8e77d9b5b70268f2f
vasamsettiamulya <vasamsettiamulya@gmail.com>
2022-11-17 20:43:37 +0530

    record sending

---
+++ vehicle.java
@@ -0,0 +1,26 @@
+record vehicle(int number,String name,int wheel)
+{
+vehicle()
+{
+this(01,"cycle",2);
+}
+public static void main(String args[])
+{
+Student S1=new Student(01,"cycle",2);
+Student S2=new Student(02, "nanocar" , 3);
+Student S3=new Student(03, "bigcar",4);
+Student S4=new Student(04, "bus" ,6);
+Student S5=new Student(05, "lorry" 8,);
+
+System.out.println(S1);
+
+System.out.println(S2);
+
+System.out.println(S3);
+
+System.out.println(S4);
+
+System.out.println(S5);
+
+}
+}
