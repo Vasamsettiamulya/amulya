f94e03e04a275985c4ed8c6a41b11c060514485b
vasamsettiamulya <vasamsettiamulya@gmail.com>
2022-11-17 20:28:48 +0530

    record sending

---
+++ recordEmployee.java
@@ -0,0 +1,26 @@
+record employee(int id,String name,float sallery)
+{
+employee()
+{
+this(01,"manga",10000);
+}
+public static void main(String args[])
+{
+Student S1=new Student(01,"manga",10000);
+Student S2=new Student(02, "vasavi",20000);
+Student S3=new Student(03, "rama",40000);
+Student S4=new Student(04, "sirisha",50000);
+Student S5=new Student(05, "jaya",60000);
+
+System.out.println(S1);
+
+System.out.println(S2);
+
+System.out.println(S3);
+
+System.out.println(S4);
+
+System.out.println(S5);
+
+}
+}
